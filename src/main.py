1b8e285f98df48eeb93dd290855fdfa12b17d4fc
enyciaa <ian.andrew.alexander@gmail.com>
2014-06-02 23:33:55 +0800

    Popup

--- src/main.py
+++ src/main.py
@@ -1,32 +1,32 @@
 '''
-Created on 12 Nov 2013
-
-@author: Mr Lahey
-
-Function
--A button moves around the screen as you click it.  Get as many clicks in a set time as possible
-
-Uses: 
--Adding and removing widgets in kv file using kv file
--Adding/removing widgets in kv file using py file
--Game phase logic, i.e starting, ending and during
--Moving a widget around a float layout
--Creating random numbers
--Calling functions from widget outside root widget
+Added
+-Mole only moves if game is active
+-Button background when clicked has been changed
 
 '''
 
 import kivy
-kivy.require('1.0.5')
+kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
-from kivy.properties import ListProperty, NumericProperty
+from kivy.properties import ListProperty, NumericProperty, ObjectProperty
 from random import randrange
+from kivy.uix.popup import Popup
 
-class Ball(Widget):
-    ball_center = ListProperty([200, 200])  #Start position of ball
+
+class StartPopUp(Popup):
+    
+    def __init__(self, controller, **kw):
+        super(StartPopUp, self).__init__(**kw)
+        self.controller = controller
+           
+    def start_click(self):
+        self.controller.start_game()                    
+
+class Mole(Widget):
+    mole_center = ListProperty([0, 0])  #Start position of ball
     
     #New position of ball      
     def random(self):   
@@ -35,11 +35,9 @@ class Ball(Widget):
         return (x, y)    
         
     def move(self):
-        self.ball_center = self.random()
-        #The reason for so many parents
-        #needs to be called with the controller widget, 
-        #not sure why, should probably be a better way
-        Controller.update_score(self.parent.parent.parent)
+        if self.parent.parent.parent.playing_label == 1:
+            self.mole_center = self.random()
+            Controller.score(self.parent.parent.parent)
 
 class Controller(Widget):
     score_label = NumericProperty(0)
@@ -47,36 +45,46 @@ class Controller(Widget):
     playing_label = NumericProperty(0)          #Intitial phase of game is off
     high_score_label = NumericProperty(0)
     
-    def restart_game(self):
+    
+    def __init__(self, **kw):
+        super(Controller, self).__init__(**kw)        
+    
+    def start_popup(self, *args):    
+        sp = StartPopUp(self)
+        sp.open() 
+            
+    def start_game(self):
         self.score_label = 0
         self.playing_label = 1
         self.time_label = 0
     
     def end_game(self):
         self.playing_label = 0 
-        self.add_widget(self.ids.Begin)     #Adding a widget in kv file from py file
+        self.start_popup()
         if self.score_label > self.high_score_label:    #Changes the high score if the current score is older than the old high score 
             self.high_score_label = self.score_label
     
-    def update_score(self):  
+    def score(self):  
         if (self.playing_label == 1):   #Only update label during game phase
             self.score_label += 1 
             
-    def update_time(self, dt):
+    def time(self):
         if (self.playing_label == 0):   #when game is in off phase
             self.time_label = 0 
         elif (self.playing_label == 1 and self.time_label < 9.9):   #when game is in on phase       
             self.time_label += 0.1  
         elif (self.playing_label == 1 and self.time_label > 9.8):   #when the time limit is reached  
-            self.end_game()
-        else:
-            print "update_time function error"                 
+            self.end_game()               
 
+    def update(self, dt): 
+        self.time() 
+           
 class MoleHuntApp(App):
 
     def build(self):
         game = Controller()
-        Clock.schedule_interval(game.update_time, 0.1)  #schedule a function to run repeatedly
+        Clock.schedule_once(game.start_popup, 1)
+        Clock.schedule_interval(game.update, 0.1)  #schedule a function to run repeatedly
         return game
 
 if __name__ == '__main__':
