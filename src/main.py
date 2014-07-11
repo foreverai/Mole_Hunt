1ebe48ec1be9d39ea13ac55124c404489fdc9b98
enyciaa <ian.andrew.alexander@gmail.com>
2014-07-11 17:49:54 +0100

    pre APK creation

--- src/main.py
+++ src/main.py
@@ -1,15 +1,14 @@
 '''
 Added
--Added character selection functionality
--Added Times up popup
--Added preliminary graphics
--Added fonts
+-Refactored code
+-Organised changeable elements
+-Organised charselect better
+-1st draft of game graphics
 
 To Do 
--Fix how charselect is done
--Refactor code
--Organise changeable elements of game
--Start creating game graphics
+-Check timer will work even if game is running slowly
+-change how databar/gamebox is sized to one central number
+-Create more advanced graphics
 
 Widget tree
 -app
@@ -64,12 +63,23 @@ class SelectBox(Widget):
     startpopup = ObjectProperty(None)   
     
     'Selectbox Image'
-    img_selectbox = StringProperty('Images/selectbox.png')  
+    img_selectbox = StringProperty('Images/selectbox.png') 
+    
+    x_center = NumericProperty(0)
+    y_center = NumericProperty(0) 
+    x_width = NumericProperty(0)
+    y_height = NumericProperty(0) 
+    
+    #Sizing and positioning in charselect.  
+    #Issues selecting through the widget tree prevent putting them in SelectBox 
 
 class CharSelect(Widget):
     startpopup = ObjectProperty(None)
     selectbox = ObjectProperty(None)
     character1 = ObjectProperty(None)
+    
+    'Number of columns to split character sprites over'
+    char_columns = NumericProperty(2)
     
     character_lst = ListProperty([])
      
@@ -77,22 +87,22 @@ class CharSelect(Widget):
         super(CharSelect, self).__init__(*args, **kwargs)
         'Put selectable characters here, also add a character widget in kv file'
         self.character_lst.append('Images/mole.png') 
-        self.character_lst.append('Images/bird.png') 
+        self.character_lst.append('Images/bird.png')   
     
+    #Position selectbox on start    
+    def start(self, *args):
+        #if these are in select box widget charselect cant be accessed from selectbox, why?
+        self.selectbox.x_width = self.character1.width
+        self.selectbox.y_height = self.character1.height 
+        self.selectbox.x_center = self.character1.center_x
+        self.selectbox.y_center = self.character1.center_y       
+    
+    #Position selectbox on character click
     def image_select(self, character):
-        #To do with relativelayout
-        #will it work for muore characters?
-        if character.x < 0:
-            x = 0
-        else:    
-            x = character.x - self.character1.x
-            
-        if  character.y < 0:
-            y = 0
-        else:
-            y = character.y - self.character1.y  
-            
-        self.selectbox.pos = x, y
+        self.selectbox.x_width = character.width
+        self.selectbox.y_height = character.height
+        self.selectbox.x_center = character.center_x
+        self.selectbox.y_center = character.center_y
         self.startpopup.app.game.mole.current_char = character.source  
 
 class StartPopUp(Popup):
@@ -102,24 +112,26 @@ class StartPopUp(Popup):
     'Size of startpopup'
     startpopup_size = ListProperty([.4, .4])
     'Image of startpopup'
-    img_startpopup = StringProperty('Images/gamebox.png') 
-               
+    img_startpopup = StringProperty('Images/popup.png') 
+    
+    #on button click start game           
     def start_click(self):
         self.app.game.start_game()  
          
 class DataBar(Widget):
     game = ObjectProperty(None)
     
+    'Image of startpopup'
+    img_databar = StringProperty('Images/databar.png') 
     'Game length'
-    game_time = NumericProperty(0.9)  #has to be one below whole number, why?
-        
-    high_score_label = NumericProperty(0)  
-    time_label = NumericProperty(0)   
-    score_label = NumericProperty(0)       
+    game_time = NumericProperty(9.9)  #has to be one below whole number, why?
     
-    def new_high_score(self):
-        if self.score_label > self.high_score_label:    #Changes the high score if the current score is higher than the old high score 
-            self.high_score_label = self.score_label  
+    score_label = NumericProperty(0)     
+    high_score_label = NumericProperty(0)  
+    time_label = NumericProperty(0)       
+               
+    def score(self):  
+        self.score_label += 1  
     
     def time(self, dt):
         cond1 = self.game.playing_label
@@ -127,16 +139,22 @@ class DataBar(Widget):
         if cond1 and cond2:
             self.time_label += 0.1
         elif cond1:
-            self.game.end_game()            
-           
-    def score(self):  
-        self.score_label += 1 
+            self.game.end_game()  
+            
+    #Changes the high score if the current score is higher than the old high score    
+    def new_high_score(self):
+        if self.score_label > self.high_score_label: 
+            self.high_score_label = self.score_label                   
                                                    
 class Mole(Widget):
     game = ObjectProperty(None)
     
     'Starting character selection'
     current_char = StringProperty('Images/mole.png')
+    
+    'Size of mole as percentage of screen (height , width)'
+    mole_size = ListProperty([0.1, 0.15])
+    
     'Distance from wall moles center cant be'
     wall_gap = NumericProperty(1.2)  #Needs to be a percentage to times the width/height of mole by 
           
@@ -146,26 +164,26 @@ class Mole(Widget):
         cond2 = self.game.playing_label
         if cond1 and cond2:
             self.move()
-            self.game.databar.score()        
-    
-    #New position of ball   
-    #kv width and kv height are declared in kv file 
-    #int methods are used to round all numbers as kv file doesn't accept lots of decimal places  
-    def random(self):   
-        rl_gap = int(self.kvwidth * self.wall_gap) #right-left side gap
-        bt_gap = int(self.kvheight * self.wall_gap) #top-bottom gap
-        right = int(self.game.gamebox.get_right() - bt_gap)
-        top = int(self.game.gamebox.get_top() - rl_gap)
-        x = randrange(rl_gap , right , 1)   
-        y = randrange(bt_gap , top , 1)       
-        return (x, y)    
-        
+            self.game.databar.score()  
+                  
     def move(self):
-        self.pos = self.random()    
-        
+        self.pos = self.random()
+    
+    #Wall gaps based on mole size     
+    #int methods are used to round all numbers as randrange doesn't accept float numbers  
+    def random(self):   
+        left = int(self.width * self.wall_gap) #left side gap
+        right = int(self.game.gamebox.get_right() - left) #right side gap
+        x = randrange(left , right , 1)  
+        bottom = int(self.height * self.wall_gap) #bottom gap
+        top = int(self.game.gamebox.get_top() - bottom) #top gap 
+        y = randrange(bottom , top , 1)       
+        return (x, y)        
+    
+    #Return mole to center of screen    
     def end(self):
-        x = self.game.kvcenter_x
-        y = self.game.kvcenter_y
+        x = self.game.width/2
+        y = self.game.height/2
         self.center = x, y           
 
 class MoleGame(Widget):
@@ -176,8 +194,9 @@ class MoleGame(Widget):
     
     'Gamebox Image'
     img_gamebox = StringProperty('Images/gamebox.png')  
+    
     'Seconds times_up pop up is visible for'  
-    timesup_time = NumericProperty(1.5)
+    timesup_time = NumericProperty(2)
     
     playing_label = BooleanProperty(False)      
                 
@@ -196,24 +215,23 @@ class MoleGame(Widget):
 class MoleHuntApp(App):
     game = ObjectProperty(None)
     startpopup = ObjectProperty(None)
-    times_up = ObjectProperty(None)
+    timesup = ObjectProperty(None)
 
     def build(self):
         self.game = MoleGame()
         self.startpopup = StartPopUp()
-        
-        Clock.schedule_once(self.start_popup_scrn, 1)
-        Clock.schedule_interval(self.game.databar.time, 0.1) #for counter label 
-        
+        Clock.schedule_once(self.start_popup_scrn, 0.5) #So it runs after game widget is created
+        Clock.schedule_interval(self.game.databar.time, 0.1) #to add to counter every 0.1 seconds 
         return self.game
     
     def times_up(self):
-        self.times_up = TimesUp()
-        self.game.add_widget(self.times_up)  
+        self.timesup = TimesUp()   #Needs declared here rather than in build otherwise initilised wrongly
+        self.game.add_widget(self.timesup)  
     
     def start_popup_scrn(self, *args):
-        self.game.remove_widget(self.times_up) 
+        self.game.remove_widget(self.timesup) 
         self.startpopup.open()   
+        Clock.schedule_once(self.startpopup.charselect.start, 0.5)   # select box can only be initilised after the pop up is created
 
 if __name__ == '__main__':
     MoleHuntApp().run()
